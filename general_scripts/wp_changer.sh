9bab114455383be695e35db38304cfa486001cbe
Fynn <enriquefynn@gmail.com>
2016-05-13 17:17:04 +0200

    better wp changer

--- general_scripts/wp_changer.sh
+++ general_scripts/wp_changer.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env zsh
 
-while true; 
-do
-    for i in `ls ~/Wallpapers/*.png ~/Wallpapers/*.jpg | sort -R`
-    do
-        hsetroot -fill $i
-        sleep $1
-    done
-done
-
+IMG=$(ls ~/Wallpapers/*.png ~/Wallpapers/*.jpg | sort -R | tail -n 1)
+hsetroot -fill $IMG
+echo $IMG
